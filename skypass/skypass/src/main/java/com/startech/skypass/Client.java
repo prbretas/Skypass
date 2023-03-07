d913feb112ca16b3c62e83aa2c0e36723b1572b0
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-03-07 00:21:48 -0300

    Client Class and API's -> OK

--- skypass/skypass/src/main/java/com/startech/skypass/Client.java
+++ skypass/skypass/src/main/java/com/startech/skypass/Client.java
@@ -1,4 +1,132 @@
 package com.startech.skypass;
 
+import java.util.Date;
+
 public class Client {
+
+    private String id;
+    private String userName;
+    private String name;
+    private String lastName;
+    private String numReg;
+    private String phone;
+    private String email;
+    private String adress ; //fk_ADRESS_CLIENT
+    private String birthday;
+
+    private boolean ativo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumReg() {
+        return numReg;
+    }
+
+    public void setNumReg(String numReg) {
+        this.numReg = numReg;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Client(String id, String userName, String name, String lastName, String numReg, String phone, String email, String adress, String birthday) {
+        this.id = id;
+        this.userName = userName;
+        this.name = name;
+        this.lastName = lastName;
+        this.numReg = numReg;
+        this.phone = phone;
+        this.email = email;
+        this.adress = adress;
+        this.birthday = birthday;
+        this.ativo = true;
+    }
+
+    public Client(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id='" + getId() + '\'' +
+                ", userName='" + getUserName() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", numReg='" + getNumReg() + '\'' +
+                ", phone='" + getPhone() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", adress='" + getAdress() + '\'' +
+                ", birthday=" + getBirthday() +
+                ", ativo=" + getAtivo() +
+                '}';
+    }
 }
--- skypass/skypass/src/main/java/com/startech/skypass/SkypassController.java
+++ skypass/skypass/src/main/java/com/startech/skypass/SkypassController.java
@@ -1,4 +1,46 @@
 package com.startech.skypass;
 
+import com.startech.skypass.Client;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@RestController
 public class SkypassController {
+    public HashMap<String, Client> clients = new HashMap<String,Client>();
+
+    @PostMapping("/client")
+    public Client adicionarCliente (@RequestBody Client c){
+        clients.put(c.getId(),c);
+        String tamanhoLista = String.valueOf(clients.size());
+        System.out.println("Cliente Cadastrado com SUCESSO!"+ " - Quantidade de Clientes: " + tamanhoLista);
+        System.out.println( c.toString());
+        return c;
+    }
+
+    @GetMapping("/client")
+    public Collection<Client> getAllClientes(){
+        return clients.values();
+    }
+
+    //@GetMapping ("/cliente/{id}/compras{idCompra}")
+    @GetMapping ("/client/{id}")
+    public Client getClientById(@PathVariable("id") String id){
+        return clients.get(id);
+    }
+
+    @PutMapping ("/client/{id}")
+    public Client atualizaCliente(@PathVariable("id") String id, @RequestBody Client c){
+        System.out.println("Cliente Alterado com SUCESSO! "+ c.getId() + " " + c.getUserName());
+        return clients.put(id,c);
+    }
+
+    @DeleteMapping ("/client/{id}")
+    public Client deletarClienteById(@PathVariable("id") String id){
+        return clients.remove(id);
+    }
+
+
+
 }
