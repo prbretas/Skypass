69af78f3f77407bce4b032f9411227ac8e0dae85
Dener Corrêa <92703482+DRC43@users.noreply.github.com>
2023-03-08 14:29:41 -0300

    Update Airport.java
    Classe Airport

--- skypass/skypass/src/main/java/com/startech/skypass/Airport.java
+++ skypass/skypass/src/main/java/com/startech/skypass/Airport.java
@@ -1,4 +1,107 @@
-package com.startech.skypass;
+import javax.print.DocFlavor;
 
 public class Airport {
+    private String id;
+    private String idAdress; // fk_Endereço_ Airport
+    private String name;
+    private String iataCode;
+    private String phone;
+    private String email;
+
+    private boolean ativo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdAdress() {
+        return idAdress;
+    }
+
+    public void setIdAdress(String idAdress) {
+        this.idAdress = idAdress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    public Airport(String id, String idAdress, String name, String iataCode, String phone, String email, boolean ativo) {
+        this.id = id;
+        this.idAdress = idAdress;
+        this.name = name;
+        this.iataCode = iataCode;
+        this.phone = phone;
+        this.email = email;
+        this.ativo = ativo;
+    }
+
+
+    public Airport() {
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "id='" + id + '\'' +
+                ", idAdress='" + idAdress + '\'' +
+                ", name='" + name + '\'' +
+                ", iataCode='" + iataCode + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", ativo=" + ativo +
+                '}';
+    }
+
+    public boolean ativar() {
+        return this.ativo = true;
+    }
+
+    public boolean inativar() {
+        return this.ativo = false;
+    }
 }
