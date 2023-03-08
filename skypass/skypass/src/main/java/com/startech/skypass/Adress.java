7df3e5f69b9364d1be42dffe1b0c5c762386f659
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-03-07 23:57:21 -0300

    add Adress Class
    Adress Class
    add , post, put, get, getbyId, delete -> Controller

--- skypass/skypass/src/main/java/com/startech/skypass/Adress.java
+++ skypass/skypass/src/main/java/com/startech/skypass/Adress.java
@@ -1,4 +1,108 @@
 package com.startech.skypass;
 
 public class Adress {
+    private String id;
+    private String adress;
+    private int number;
+    private String city;
+    private String state;
+    private String stateCode;
+    private String country;
+    private boolean ativo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean getAtivo() {
+        return ativo;
+    }
+
+    public Adress(String id, String adress, int number, String city, String state, String stateCode, String country) {
+        this.id = id;
+        this.adress = adress;
+        this.number = number;
+        this.city = city;
+        this.state = state;
+        this.stateCode = stateCode;
+        this.country = country;
+        this.ativo = true;
+    }
+    public Adress() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Adress{" +'\'' +
+                "id=" + getId() + '\'' +
+                "adress=" + getAdress() + '\'' +
+                "number=" + getNumber() +
+                "city=" + getCity() + '\'' +
+                "state=" + getState() + '\'' +
+                "stateCode=" + getStateCode() + '\'' +
+                "country=" + getCountry() + '\'' +
+                '}';
+    }
+
+    public boolean ativar(){
+        return this.ativo = true;
+    }
+
+    public boolean inativar(){
+        return this.ativo = false;
+    }
+
 }
--- skypass/skypass/src/main/java/com/startech/skypass/SkypassController.java
+++ skypass/skypass/src/main/java/com/startech/skypass/SkypassController.java
@@ -8,9 +8,11 @@ import java.util.HashMap;
 
 @RestController
 public class SkypassController {
+
+    //--------------------------------------------------------------------------------------------
     public HashMap<String, Client> clients = new HashMap<String,Client>();
 
-    @PostMapping("/client")
+    @PostMapping("/clients")
     public Client adicionarCliente (@RequestBody Client c){
         clients.put(c.getId(),c);
         String tamanhoLista = String.valueOf(clients.size());
@@ -19,27 +21,70 @@ public class SkypassController {
         return c;
     }
 
-    @GetMapping("/client")
+    @GetMapping("/clients")
     public Collection<Client> getAllClientes(){
         return clients.values();
     }
 
-    //@GetMapping ("/cliente/{id}/compras{idCompra}")
-    @GetMapping ("/client/{id}")
+    //@GetMapping ("/clients/{id}/compras{idCompra}")
+    @GetMapping ("/clients/{id}")
     public Client getClientById(@PathVariable("id") String id){
         return clients.get(id);
     }
 
-    @PutMapping ("/client/{id}")
+    @PutMapping ("/clients/{id}")
     public Client atualizaCliente(@PathVariable("id") String id, @RequestBody Client c){
         System.out.println("Cliente Alterado com SUCESSO! "+ c.getId() + " " + c.getUserName());
         return clients.put(id,c);
     }
-
-    @DeleteMapping ("/client/{id}")
+    @DeleteMapping ("/clients/{id}")
     public Client deletarClienteById(@PathVariable("id") String id){
+        System.out.println("Cliente Deletado com SUCESSO!");
         return clients.remove(id);
     }
+    //--------------------------------------------------------------------------------------------
+
+    public HashMap<String, Adress> adresses = new HashMap<String,Adress>();
+
+    @PostMapping("/adresses")
+    public Adress adicionarAdress (@RequestBody Adress a){
+        adresses.put(a.getId(),a);
+        String tamanhoLista = String.valueOf(adresses.size());
+        System.out.println("Endereço Cadastrado com SUCESSO!"+ " - Quantidade de Endereços: " + tamanhoLista);
+        System.out.println( a.toString());
+        return a;
+    }
+
+    @GetMapping("/adresses")
+    public Collection<Adress> getAllEndereços(){
+        return adresses.values();
+    }
+
+    @GetMapping ("/adresses/{id}")
+    public Adress getAdressById(@PathVariable("id") String id){
+        return adresses.get(id);
+    }
+
+    @PutMapping ("/adresses/{id}")
+    public Adress atualizaAdress(@PathVariable("id") String id, @RequestBody Adress a){
+        System.out.println("Endereço Alterado com SUCESSO! "+ a.getId() + " " + a.getAdress());
+        return adresses.put(id,a);
+    }
+
+    @DeleteMapping ("/adresses/{id}")
+    public Adress deletarAdressById(@PathVariable("id") String id){
+        System.out.println("Endereço Deletado com SUCESSO!");
+        return adresses.remove(id);
+    }
+    //--------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 
 
 
