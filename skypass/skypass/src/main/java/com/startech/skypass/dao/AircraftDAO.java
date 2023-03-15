877832ff3dd4929abd5e0cfc3d6bc861788236e9
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-03-15 14:55:47 -0300

    add numSeats

--- skypass/skypass/src/main/java/com/startech/skypass/dao/AircraftDAO.java
+++ skypass/skypass/src/main/java/com/startech/skypass/dao/AircraftDAO.java
@@ -20,12 +20,36 @@ public class AircraftDAO {
     private String infoSystem;
     private String latitude;
     private String longitude;
+    private int numSeats;
+    @Column(name = "Eco_Seats")
+    private int numEconomicSeats;
+    @Column(name = "Exe_Seats")
+    private int numExecutiveSeats;
+    @Column(name = "FC_Seats")
+    private int numFirstClassSeats;
     private double cargoWeight;
     private boolean ativo;
 
-    public AircraftDTO toDTO(){
-        return AircraftDTO.
-                builder()
+    public int calcularSeatsClass(int numSeats){
+        this.numSeats = numSeats;
+        this.numEconomicSeats = (int) (numSeats * 0.7);
+        this.numExecutiveSeats = (int) (numSeats * 0.2);
+        this.numFirstClassSeats = (int) (numSeats * 0.1);
+        return  numSeats;
+    }
+
+    public void mostrarSeatsClass(){
+        System.out.println("\nA quantidade de poltranas são:"
+                + "\n- Economica: " + getNumEconomicSeats()
+                + "\n- Executiva: " + getNumExecutiveSeats()
+                + "\n- Primeira Classe: " + getNumFirstClassSeats()
+                + "\n- Total de Poltronas: " + getNumSeats());
+    }
+
+
+
+    public AircraftDTO toDTO (){
+        return AircraftDTO.builder()
                 .id(id)
                 .idAirline(idAirline)
                 .model(model)
@@ -33,11 +57,14 @@ public class AircraftDAO {
                 .infoSystem(infoSystem)
                 .latitude(latitude)
                 .longitude(longitude)
+                .numSeats(calcularSeatsClass(numSeats))
+                .numEconomicSeats(numEconomicSeats)
+                .numExecutiveSeats(numExecutiveSeats)
+                .numFirstClassSeats(numFirstClassSeats)
                 .cargoWeight(cargoWeight)
-                .ativo(ativar())
+                .ativo(ativo)
                 .build();
     }
-
     public boolean ativar(){
         return this.ativo = true;
     }
