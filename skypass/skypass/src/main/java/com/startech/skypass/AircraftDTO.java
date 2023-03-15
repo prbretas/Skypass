32d6e246e31f2bc9a8b85c5b6969459d13d30cc2
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-03-15 14:54:53 -0300

    add numSeats

--- skypass/skypass/src/main/java/com/startech/skypass/AircraftDTO.java
+++ skypass/skypass/src/main/java/com/startech/skypass/AircraftDTO.java
@@ -17,22 +17,46 @@ public class AircraftDTO {
     private String infoSystem;
     private String latitude;
     private String longitude;
+    private int numSeats;
+    private int numEconomicSeats;
+    private int numExecutiveSeats;
+    private int numFirstClassSeats;
     private double cargoWeight;
     private boolean ativo;
 
+ public int calcularSeatsClass(int numSeats){
+        this.numSeats = numSeats;
+        this.numEconomicSeats = (int) (numSeats * 0.7);
+        this.numExecutiveSeats = (int) (numSeats * 0.2);
+        this.numFirstClassSeats = (int) (numSeats * 0.1);
+        return  numSeats;
+    }
+
+    public void mostrarSeatsClass(){
+        System.out.println("\nA quantidade de poltranas são:"
+                + "\n- Economica: " + getNumEconomicSeats()
+                + "\n- Executiva: " + getNumExecutiveSeats()
+                + "\n- Primeira Classe: " + getNumFirstClassSeats()
+                + "\n- Total de Poltronas: " + getNumSeats());
+    }
 
     @Override
     public String toString() {
-        return "Aircraft{" + '\'' +
-                "id=" + getId() + '\'' +
-                "idAirline=" + getIdAirline() + '\'' +
-                "model='" + getModel() + '\'' +
-                "numSerie='" + getNumSerie() + '\'' +
-                "infoSystem='" + getInfoSystem() + '\'' +
-                "latitude='" + getLatitude() + '\'' +
-                "longitude='" + getLongitude() + '\'' +
-                "cargoWeight=" + getCargoWeight() +
-                '}';
+        return "\nAircraft{" +
+                "\nid=" + getId() +
+                "\nidAirline=" + getIdAirline() +
+                "\nmodel='" + getModel() +
+                "\nnumSerie='" + getNumSerie() +
+                "\ninfoSystem='" + getInfoSystem() +
+                "\nlatitude='" + getLatitude() +
+                "\nlongitude='" + getLongitude() +
+                "\nnumSeats=" + getNumSeats() +
+                "\nnumEconomicSeats=" + getNumEconomicSeats() +
+                "\nnumExecutiveSeats=" + getNumExecutiveSeats() +
+                "\nnumFirstClassSeats=" + getNumFirstClassSeats() +
+                "\ncargoWeight=" + getCargoWeight() +
+                "\nativo=" + ativo +
+                "\n}";
     }
 
     public AircraftDAO toDAO(){
@@ -45,6 +69,10 @@ public class AircraftDTO {
                 .infoSystem(infoSystem)
                 .latitude(latitude)
                 .longitude(longitude)
+                .numSeats(calcularSeatsClass(numSeats))
+                .numEconomicSeats(numEconomicSeats)
+                .numExecutiveSeats(numExecutiveSeats)
+                .numFirstClassSeats(numFirstClassSeats)
                 .cargoWeight(cargoWeight)
                 .ativo(ativar())
                 .build();
